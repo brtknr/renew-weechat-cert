21fe1005ce6781d530784237d412b6d83999f0a1
Bharat Kunwar <brtknr@bath.edu>
2021-05-28 13:22:53 +0000

    Specify relay.pem

--- renew-weechat-cert.sh
+++ renew-weechat-cert.sh
@@ -5,12 +5,14 @@ export DOMAIN=${1:-chat.rawn.uk}
 export EMAIL=${2:-brtknr@bath.edu}
 
 sudo certbot renew --cert-name ${DOMAIN} --pre-hook 'systemctl stop nginx' --post-hook 'systemctl start nginx' || sudo certbot certonly --manual --preferred-challenges=dns -d ${DOMAIN} -m ${EMAIL}
-sudo cat /etc/letsencrypt/live/${DOMAIN}/{fullchain,privkey}.pem > ~/.weechat/ssl/relay.pem
-
-echo "*/set weechat.network.gnutls_ca_file '/etc/ssl/certs/ca-certificates.crt'" > ~/.weechat/weechat_fifo
+sudo cat /etc/letsencrypt/live/${DOMAIN}/{fullchain,privkey}.pem > ~/.weechat/certs/relay.pem
+echo "*/set weechat.network.gnutls_ca_file /etc/ssl/certs/ca-certificates.crt" > ~/.weechat/weechat_fifo
+echo "*/set relay.network.ssl_cert_key %h/certs/relay.pem" > ~/.weechat/weechat_fifo
 echo "*/relay sslcertkey" > ~/.weechat/weechat_fifo
-(crontab -l; echo "0 3 * * * $(readlink -f $0) ${DOMAIN}") | uniq | crontab
 echo "*/reconnect" > ~/.weechat/weechat_fifo
+echo "*/save" > ~/.weechat/weechat_fifo
+
+(crontab -l; echo "0 3 * * * $(readlink -f $0) ${DOMAIN}") | uniq | crontab
 
 if [[ ! -f /etc/systemd/system/weechat.service ]]; then
     cat << EOF | sudo tee /etc/systemd/system/weechat.service
